edbad865bc2a8471b1b8891e25d96b4d9a745fd5
iyan31 <yantisa@gmail.com>
2020-05-31 11:11:18 +0700

    remove path from the executables

--- scriptosm.py
+++ scriptosm.py
@@ -8,14 +8,14 @@ poly_files = ['Aceh', 'SumateraUtara', 'SumateraBarat', 'Riau', 'KepulauanRiau',
               'MalukuUtara', 'SulawesiUtara', 'Gorontalo', 'SulawesiTengah', 'SulawesiBarat', 'SulawesiSelatan',
               'SulawesiTenggara', 'Papua', 'PapuaBarat']
 
+# downloading pbf data in geofabrik
+downloadpbf = "wget -N http://download.geofabrik.de/asia/indonesia-latest.osm.pbf"
+os.system(downloadpbf)
 
-for poly in poly_files:    
-    # downloading pbf data in geofabrik
-    downloadpbf = "wget -N http://download.geofabrik.de/asia/indonesia-latest.osm.pbf"
-    os.system(downloadpbf)
+for poly in poly_files:
     
     # clipping data into pbf data
-    clip2province = "/usr/sbin/osmconvert indonesia-latest.osm.pbf -B=/var/www/html/data/poly/{0}.poly -o=pbf/{0}.pbf".format(poly)
+    clip2province = "osmconvert indonesia-latest.osm.pbf -B=data/poly/{0}.poly -o=pbf/{0}.pbf".format(poly)
     os.system(clip2province)
 
     # Get permission pbf data in server
@@ -23,9 +23,9 @@ for poly in poly_files:
     os.system(permission)
 
     # filtering osm data based on building
-    filterbuilding = "bin/osmosis --read-pbf pbf/{0}.pbf --tf accept-ways building=* --tf reject-nodes --tf reject-relations --write-pbf pbf/{0}_building.pbf".format(poly)
+    filterbuilding = "osmosis --read-pbf pbf/{0}.pbf --tf accept-ways building=* --tf reject-nodes --tf reject-relations --write-pbf pbf/{0}_building.pbf".format(poly)
     os.system(filterbuilding)
 
     # showing osm data statistic
-    osmstats = "/usr/sbin/osmconvert pbf/{0}_building.pbf --out-statistics > stat/{0}.stat".format(poly)
+    osmstats = "osmconvert pbf/{0}_building.pbf --out-statistics > stat/{0}.stat".format(poly)
     os.system(osmstats)
