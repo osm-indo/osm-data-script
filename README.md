67fddd379175af991a9771395a5aa4ec677e5955
iyan31 <yantisa@gmail.com>
2020-06-01 16:02:09 +0700

    autodetect poly directory content

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # osm-data-script
-This script build to parse OSM pbf data by admin boundary and specific tags. This tool using OSMOSIS and OSMCONVERT as the main tools to run the script.
+This script was developed to address a problem where a country level PBF is still too big to download or analysed. Thus the purpose of the script is to clip OSM PBF data by sub national admin boundaries (in .poly format). The result of this script can be accessed at the OSM Indonesia Data ([English](https://openstreetmap.id/en/data-openstreetmap-indonesia/) | [Indonesia](https://https://openstreetmap.id/data-openstreetmap-indonesia/)) page.
+
+It is recommended to run the command in cron jobs to automate the process periodically.
+
+An additional Osmosis command performed to count how many buildings within each clipped PBF file to provide overview of how many buildings have been mapped per province. The simple graph of the result can be accessed at Building Statistic Report ([English](https://openstreetmap.id/data/osmstatsbar_en.html) | [Indonesia](https://openstreetmap.id/data/osmstatsbar_id.html)) page.
+
+## Requirements
+- [Osmosis](https://wiki.openstreetmap.org/wiki/Osmosis), to chop the PBF
+- [Osmconvert](https://wiki.openstreetmap.org/wiki/Osmconvert), to count the buildings inside the PBF
+- Poly files (put it all under `poly` folder), the script will automatically split based on the content of this directory
+
+## Start the script
+`$ python scriptosm.py`
--- scriptosm.py
+++ scriptosm.py
@@ -1,38 +1,42 @@
 import os
 import sys
 
-poly_files = ['Aceh', 'SumateraUtara', 'SumateraBarat', 'Riau', 'KepulauanRiau', 'Jambi', 'KepulauanBangkaBelitung',
-              'Bengkulu', 'SumateraSelatan', 'Lampung', 'Banten', 'JawaBarat', 'Jakarta',
-              'JawaTengah', 'JawaTimur', 'Yogyakarta', 'KalimantanBarat', 'KalimantanTengah',
-              'KalimantanTimur', 'KalimantanSelatan', 'KalimantanUtara', 'Bali', 'NTB', 'NTT', 'Maluku',
-              'MalukuUtara', 'SulawesiUtara', 'Gorontalo', 'SulawesiTengah', 'SulawesiBarat', 'SulawesiSelatan',
-              'SulawesiTenggara', 'Papua', 'PapuaBarat']
+polypath = 'poly/'
+poly_files = []
+
+#list all poly files in the poly directory using os.listdir
+for entry in os.listdir(polypath):
+    if os.path.isfile(os.path.join(polypath, entry)) and not entry.startswith('.'):
+        stripext = os.path.splitext(entry)[0]
+        poly_files.append(stripext)
+
+print "Detected poly files :", str(poly_files)[1:-1]
 
 #check if pbf and stat folder exist, otherwise create it
 if not os.path.exists('pbf'):
     os.makedirs('pbf')
 
-if not os.path.exists('stat`'):
+if not os.path.exists('stat'):
     os.makedirs('stat')
 
 # downloading pbf data in geofabrik
-downloadpbf = "wget -N http://download.geofabrik.de/asia/indonesia-latest.osm.pbf"
-os.system(downloadpbf)
+# downloadpbf = "wget -N http://download.geofabrik.de/asia/indonesia-latest.osm.pbf"
+# os.system(downloadpbf)
 
 for poly in poly_files:
 
     # clipping data into pbf data
-    clip2province = "osmconvert indonesia-latest.osm.pbf -B=data/poly/{0}.poly -o=pbf/{0}.pbf".format(poly)
+    clip2province = "osmconvert indonesia-latest.osm.pbf -B=poly/{0}.poly -o=pbf/{0}.pbf".format(poly)
     os.system(clip2province)
 
-    # Get permission pbf data in server
-    permission = "chmod +r pbf/*"
+    # give read permission to pbf data
+    permission = "chmod +r pbf/{0}.pbf".format(poly)
     os.system(permission)
 
-    # filtering osm data based on building
-    filterbuilding = "osmosis --read-pbf pbf/{0}.pbf --tf accept-ways building=* --tf reject-nodes --tf reject-relations --write-pbf pbf/{0}_building.pbf".format(poly)
+    # filtering osm data based on building tag
+    filterbuilding = "osmosis --rb pbf/{0}.pbf --tf accept-ways building=* --tf reject-nodes --tf reject-relations --wb pbf/{0}_building.pbf".format(poly)
     os.system(filterbuilding)
 
-    # showing osm data statistic
+    # count the building data and put it on stat file
     osmstats = "osmconvert pbf/{0}_building.pbf --out-statistics > stat/{0}.stat".format(poly)
     os.system(osmstats)
